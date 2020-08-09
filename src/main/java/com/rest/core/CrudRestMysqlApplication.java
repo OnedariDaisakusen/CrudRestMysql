8d2a6ccaf7cdc15eeaf56339f3f6f5c460c8e580
OnedariDaisakusen <novagit666@gmail.com>
2020-08-08 22:15:29 -0500

    Primer commir

---
+++ src/main/java/com/rest/core/CrudRestMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.rest.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudRestMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudRestMysqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rest/core/CrudRestMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudRestMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
