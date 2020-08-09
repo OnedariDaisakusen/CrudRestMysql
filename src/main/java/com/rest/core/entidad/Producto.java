5662f1d1cea13c477cd6e2d29bde352296aed974
OnedariDaisakusen <novagit666@gmail.com>
2020-08-08 22:40:12 -0500

    Segundo Commit

--- src/main/java/com/rest/core/entidad/Producto.java
+++ src/main/java/com/rest/core/entidad/Producto.java
@@ -1,10 +1,23 @@
 package com.rest.core.entidad;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "producto")
 public class Producto {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column
+	private int idproducto;
+	
+	
+	
+	
+	
 }
